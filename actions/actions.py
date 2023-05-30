5ecf62dc93234ca0c3919ea24a2ef21f055ba4a0
laroshan <laroshanlaro@gmail.com>
2023-05-30 21:21:57 +0530

    Committing for backup

--- actions/actions.py
+++ actions/actions.py
@@ -7,13 +7,18 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 from typing import Any, Text, Dict, List
-
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+#
+#
+client = MongoClient("mongodb+srv://laro-31:laro-31@courselist.i0n97yv.mongodb.net/?retryWrites=true&w=majority")
+db = client["courselist"]
+collection = db["courselist"]
 
-#
-#
+
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
@@ -22,6 +27,8 @@ class ActionHelloWorld(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        institute = tracker.get_slot('institute')
+        dispatcher.utter_message(text='The features you entered: ' + str(institute))
         dispatcher.utter_message(text="Hello World!")
 
         return []
@@ -36,5 +43,77 @@ class ActionZScore(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(text="Zscore is 1.567")
+
+        return []
+
+
+class ActionDefaultFallback(Action):
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Sorry! Can you check on what you are asking for")
+
+        return []
+
+
+class ActionCourse(Action):
+
+    def name(self):
+        return "action_course"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Retrieve data from MongoDB
+        course = tracker.get_slot("course")
+        dispatcher.utter_message(course)
+        print("Identified slot is" + course)
+        # course = tracker.latest_message.get("text")
+        data = collection.find_one({"course": course})
+        # Do something with the data
+        if data:
+            # Send a response message to the user
+            dispatcher.utter_message(text="Here is some data from MongoDB: {}".format(data))
+        else:
+            # Send an error message if no data is found
+            dispatcher.utter_message(text="Sorry, no data found in MongoDB.")
+        return []
+
+
+class QueryZscoreAction(Action):
+    def name(self) -> Text:
+        return "action_query_zscore"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Get the values of the slots
+        institute = tracker.get_slot("institute")
+        district = tracker.get_slot("district")
+        course = tracker.get_slot("course")
+        year = tracker.get_slot("year")
+
+        # Connect to the MongoDB database
+        try:
+            # client = MongoClient("mongodb://localhost:27017/")
+            # db = client["courselist"]
+            zscore_collection = db["zscore_list"]
+        except ConnectionFailure:
+            dispatcher.utter_message(text="Could not connect to the database.")
+            return []
+
+        # Query the z-score from the database
+        query = {"institute": institute, "district": district, "course": course, "year": year}
+        result = zscore_collection.find_one(query)
+        if result is None:
+            dispatcher.utter_message(text="No z-score found for the given criteria.")
+        else:
+            zscore = result["zscore"]
+            dispatcher.utter_message(text=f"The z-score is {zscore}.")
 
         return []
